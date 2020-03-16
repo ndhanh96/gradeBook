13683b8790ffc44ef191a5766e8819e1b973deab
Nguyen Duy Hanh <ndhanh96@gmail.com>
2020-03-16 12:41:17 +0700

    len lan 2

--- test/gradeBook.Tests/TypeTest.cs
+++ test/gradeBook.Tests/TypeTest.cs
@@ -4,9 +4,65 @@ using Xunit;
 namespace gradeBook.Tests
 {
     public class TypeTest
-    {
+    {   
+        [Fact]
+        public void TestName()
+        {
+        //Given
+            string name = "Scott";
+            string upper = MakeUpperCase(name);
+        //When
+        
+        //Then
+            Assert.Equal("Scott", name);
+            Assert.Equal("SCOTT", upper);
+        }
+
+        private string MakeUpperCase(string parameter)
+        {
+            return parameter.ToUpper();
+        }
+
         [Fact]
         public void Test1()
+        {
+        //Given
+            var x = GetInt();
+            SetInt(out x);
+            
+        //When
+        
+        //Then
+            Assert.Equal(42, x);
+        }
+
+        private void SetInt(out int x)
+        {
+            x = 42;
+        }
+
+        private int GetInt()
+        {
+            return 3;
+        }
+
+        [Fact]
+        public void CSharpCanPassByRef()
+        {   
+            var book1 = GetBook("Book 1");
+            GetBookSetName(out book1, "New Name");
+            
+            Assert.Equal("New Name", book1.Name);
+            
+        }
+
+        private void GetBookSetName(out Book book, string name)
+        {
+            book = new Book(name);
+        }
+
+        [Fact]
+        public void CSharpIsPassByValue()
         {   
             var book1 = GetBook("Book 1");
             GetBookSetName(book1, "New Name");
